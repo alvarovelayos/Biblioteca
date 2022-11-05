8625508ee889acbe250db5ff7b19425a64e11961
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-05 20:28:54 +0100

    Creada la clase autor

---
+++ src/Author.java
@@ -0,0 +1,48 @@
+public class Author {
+
+    private Integer id;
+    private String name;
+    private String surnames;
+    private String birthDate;
+    private String placeOfBirth;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+}
