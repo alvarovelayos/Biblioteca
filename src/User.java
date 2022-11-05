bc92e3619dc8f7109ec994d3c703bb0802379342
alvarovelayos <alvarovelayos31@gmail.com>
2022-11-05 20:33:37 +0100

    Creada la clase usuario

---
+++ src/User.java
@@ -0,0 +1,57 @@
+public class User {
+
+    private Integer id;
+    private String name;
+    private String surnames;
+    private Integer phone;
+    private String direction;
+    private String population;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+}
