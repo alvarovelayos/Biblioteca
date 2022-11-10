04558d93daf5ac35791fb7463af811d2360b7ab8
Alvaro <alvarovelayos31@gmail.com>
2022-11-10 16:36:08 +0100

    modificado main

--- src/Library.java
+++ src/Library.java
@@ -9,6 +9,7 @@ public class Library {
     private String publicationDate;
 
     public Integer getIsbn() {
+
         return isbn;
     }
 
@@ -17,34 +18,42 @@ public class Library {
     }
 
     public String getTitle() {
+
         return title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
     public String getAuthor() {
+
         return author;
     }
 
     public void setAuthor(String author) {
+
         this.author = author;
     }
 
     public String getGenre() {
+
         return genre;
     }
 
     public void setGenre(String genre) {
+
         this.genre = genre;
     }
 
     public String getPublicationDate() {
+
         return publicationDate;
     }
 
     public void setPublicationDate(String publicationDate) {
+
         this.publicationDate = publicationDate;
     }
 }
